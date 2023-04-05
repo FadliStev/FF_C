2c256e5f4b3bf1aa718299026bfd218f36b8c3fb
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2023-04-05 16:05:39 +0700

    Add project files.

---
+++ Exercise1_PABD/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Exercise1_PABD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID : ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password : ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database tujuan : ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke Database : ");
+                    char cr = Convert.ToChar(Console.ReadLine());
+                    switch (cr)
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source =  FADLISTEV\\FADLI036; " +
+                                    "initial catalog = {0}; " +
+                                    "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat Seluruh Data");
+                                        Console.WriteLine("2. Tambah Data");
+                                        Console.WriteLine("3. Keluar");
+                                        Console.WriteLine("\n Enter your Choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+
+                                        switch (ch)
+                                        {
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("DATA MAHASISWA\n");
+                                                    Console.WriteLine();
+                                                    pr.baca(conn);
+
+                                                }
+                                                break;
+                                            case '2':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("Input Data Mahasiswa\n");
+                                                    Console.WriteLine("Masukkan NIM : ");
+                                                    string NIM = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Mahasiswa : ");
+                                                    string NmaMhs = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Alamat Mahasiswa : ");
+                                                    string Almt = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan jenis kelamin (L/P) : ");
+                                                    string jk = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No Telepon : ");
+                                                    string notlpn = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        pr.insert(NIM, NmaMhs, Almt, jk, notlpn, conn);
+                                                        conn.Close();
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki" + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            case '3':
+                                                conn.Close();
+                                                return;
+                                            default:
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("\nInvalid Option");
+                                                }
+                                                break;
+                                        }
+
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nCheck for the value entered. ");
+                                    }
+                                }
+
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid Option");
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tidak dapat mengakses database menggunakan user tersebut\n");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void baca(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("Select*From HRD.Mahasiswa", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void insert(string NIM, string NmaMhs, string Almt, string jk, string notlpn, SqlConnection con)
+        {
+            string str = "";
+            str = "insert into HRD.MAHASISWA (NIM, NamaMhs, Sex, PhoneMhs)" + "values (@nim, @nma, @alamat, @JK, @Phn)";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("nim", NIM));
+            cmd.Parameters.Add(new SqlParameter("nma", NmaMhs));
+            cmd.Parameters.Add(new SqlParameter("alamat", Almt));
+            cmd.Parameters.Add(new SqlParameter("JK", jk));
+            cmd.Parameters.Add(new SqlParameter("Phn", notlpn));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Ditambahkan");
+        }
+    }
+}
